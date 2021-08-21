cfbf960ed22f1cad803b02c0869eee5c0fb6b3ed
zarif-al <zarif_al96@outlook.com>
2021-08-21 17:11:39 +0600

    moved index page styling to global, framer-motion issue

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Container from "react-bootstrap/Container";
 import { motion } from "framer-motion";
 import TabContent from "../components/tabContent";
 import React, { useState } from "react";
 export default function Home() {
   return (
-    <Container className={styles.container} fluid>
+    <Container className="container" fluid>
       <motion.div
-        className={styles.header}
+        className="header"
         initial={"hidden"}
         animate={"visible"}
         exit={"exit"}
